487c791d4086f04f77b3440f8e53ecd947ae2205
shraddha-cpu <shraddhapaygude5337@gmail.com>
2022-09-08 17:40:16 +0530

    for loop

---
+++ shraddha/Class13.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shraddha
+{
+    internal class Class13
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=10;i++)
+            {
+                Console.WriteLine(i);
+            }
+            for( int i=10;i>=1;i--)
+                {
+                    Console.WriteLine(i);
+                }
+        }
+    }
+}
